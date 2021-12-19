b906d02b3d315a49542f4b4d09c7253f86dc7138
SayedS7S <elsayed.7amada@gmail.com>
2021-12-19 11:10:17 +0200

    Done

--- clock.py
+++ clock.py
@@ -2,7 +2,20 @@ from tkinter import *
 from tkinter.ttk import *
 from time import strftime
 
+# Create Window
 WINDOW = Tk()
 WINDOW.title("Clock")
 WINDOW.config(background="black")
 WINDOW.geometry("800x100")
+# get time
+def getTime():
+    time = strftime("%H:%M:%S %p")
+    label.config(text=time)
+    label.after(1000, getTime)
+
+label = Label(WINDOW, font=("ds-digital", 80), background="black", foreground="purple")
+label.pack(anchor="center")
+
+getTime()
+
+mainloop()
